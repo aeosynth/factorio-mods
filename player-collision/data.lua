87a42cd667b655c96cce4d369fab1294052c0357
James Campos <james.r.campos@gmail.com>
2023-01-30 01:00:12 -0800

    player collision

---
+++ player-collision/data.lua
@@ -0,0 +1,2 @@
+local n = settings.startup["pc-radius"].value
+data.raw["character"]["character"].collision_box = {{-n, -n}, {n, n}}
---
+++ player-collision/settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+  {
+    name = "pc-radius",
+    type = "double-setting",
+    default_value = 0.1,
+    minimum_value = 0,
+    maximum_value = 0.5,
+    setting_type = "startup"
+  }
+})
